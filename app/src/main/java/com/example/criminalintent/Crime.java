7e8f05b7aae0c15e910f4a66f49222e5b245b26d
sit8788 <papyrin@gmail.com>
2020-04-08 16:16:26 +0300

    part 7

---
+++ app/src/main/java/com/example/criminalintent/Crime.java
@@ -0,0 +1,45 @@
+package com.example.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+
+    public Crime(UUID id, Date date) {
+        mId = id;
+        mDate = date;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    private boolean mSolved;
+}
