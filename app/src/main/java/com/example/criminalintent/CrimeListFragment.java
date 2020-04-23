4810934a37b6a42aa56285044174b643b3624ae2
sit8788 <papyrin@gmail.com>
2020-04-23 16:25:07 +0300

    Part 11

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.java
@@ -65,7 +65,7 @@ public class CrimeListFragment extends Fragment {
 /*            Toast.makeText(getActivity(),
                     mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT)
                     .show();*/
-            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            Intent intent = CrimePagerActivity.newIntent(getActivity(), mCrime.getId());
             startActivity(intent);
         }
     }
